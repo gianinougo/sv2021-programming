6d8585ab2b05c2279830e7ec6e85321c098e352e
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-09-27 09:44:29 +0200

    Ej.3: División con datos prefijados

---
+++ tema01-contactoConCSharp/003-Division.cs
@@ -0,0 +1,13 @@
+﻿// © Jorge (...)
+// Dividir 8765 entre 234
+
+using System;
+
+class DivisionPrefijada
+{
+    static void Main()
+    {
+        Console.WriteLine("El resultado de dividir 8765 entre 234 es: ");
+        Console.WriteLine(8765/234);
+    }
+}
