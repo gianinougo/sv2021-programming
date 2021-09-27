4eed3d0ce6705bc8820c73639644b84285ca602e
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-09-27 09:45:54 +0200

    Ej.005: Prioridades

---
+++ tema01-contactoConCSharp/005-Prioridades.cs
@@ -0,0 +1,22 @@
+﻿// Ejercicio 5:
+// Calcula el resultado de (48 + 14 / 5 + 15 * 6 ) % 9 
+//      y comprueba el resultado a mano
+// Javier (...)
+
+using System;
+
+class Operacion
+{
+    static void Main()
+    {
+        int x;
+
+        x = (48 + 14 / 5 + 15 * 6) % 9;
+
+        Console.WriteLine("Resultado según C# : {0}", x);
+        Console.WriteLine("Resultado a mano :");
+        Console.WriteLine("  (48 + 2 + 90) % 9");
+        Console.WriteLine("  140 % 9");
+        Console.WriteLine("  5");
+    }
+}
