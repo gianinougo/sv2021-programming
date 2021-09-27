0550d7f48128fa4b79aeb9e2cbc53d1c0a0eeb87
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-09-27 09:46:43 +0200

    Ej.006: Pies a milímetros

---
+++ tema01-contactoConCSharp/006-PiesAMilimetros.cs
@@ -0,0 +1,32 @@
+﻿// Virginia (...)
+// Conversor de pies a milímetros
+
+
+/*
+Debe usar "Write" en vez de "{0}". 
+Debe utilizar "using System;". 
+Debe  contener dos comentarios de una línea al principio: 
+ - Uno con tu nombre
+ - Otro con el cometido del programa 
+*/
+
+using System;
+
+class Ejercicio6
+{
+    static void Main()
+    {
+        int pies;
+        int milimetros;
+        int milimetrosPorPie = 305;
+
+        Console.Write("Introduce el número de pies: ");
+        pies = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write(pies);
+        Console.Write(" pies son ");
+        milimetros = milimetrosPorPie * pies;
+        Console.Write(milimetros);
+        Console.WriteLine(" milímetros.");
+    }
+ }
