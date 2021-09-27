df7a0b857aee53df4369051f0ecca2a9fdb40667
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-09-27 09:45:15 +0200

    Ej.004: Módulo, con datos prefijados

---
+++ tema01-contactoConCSharp/004-Modulo.cs
@@ -0,0 +1,18 @@
+/*
+Haz un programa que calcule el resto de la división de 577 entre 13.
+Haz la división a mano y comprueba el resultado.
+*/
+
+// Por Franco (...)
+
+using System;
+
+class Ejercicio4
+{
+	static void Main()
+    {
+		int resultado;
+		resultado = 577 % 13;
+		Console.WriteLine(resultado);
+	}
+}
