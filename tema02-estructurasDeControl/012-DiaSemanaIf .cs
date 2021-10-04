7a36503bcef5c6dac8f6014ea00e78f0c3051fee
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-10-04 09:35:17 +0200

    Ej.012: Día de la semana, if

---
+++ tema02-estructurasDeControl/012-DiaSemanaIf .cs
@@ -0,0 +1,24 @@
+//Tema 2 Semana 1 Recomendado 12 
+// Ezequiel (...)
+//Devuelve el nombre de un día de la semana a partir de su número, usando if
+using System;
+
+public class T2s1r12
+{
+    public static void Main()
+    {
+        int diaSemana;
+        
+        Console.WriteLine("Dime un día de la semana (entre 1 y 7)");
+        diaSemana = Convert.ToInt32(Console.ReadLine());
+        
+        if(diaSemana == 1) Console.WriteLine("Es Lunes.");
+        else if(diaSemana == 2) Console.WriteLine("Es Martes.");
+        else if(diaSemana == 3) Console.WriteLine("Es Miércoles.");
+        else if(diaSemana == 4) Console.WriteLine("Es Jueves.");
+        else if(diaSemana == 5) Console.WriteLine("¡Es Viernes!.");
+        else if(diaSemana == 6) Console.WriteLine("¡Es Sabado!.");
+        else if(diaSemana == 1) Console.WriteLine("¡Es Domingo!.");
+        else Console.WriteLine("El número debe estar entre 1 y 7.");
+    }
+}
