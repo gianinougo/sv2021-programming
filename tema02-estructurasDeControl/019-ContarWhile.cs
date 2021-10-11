305d8ef77f71a16cf2e7ec80b2fb6b61a10c0c4c
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-10-11 09:26:15 +0200

    Ej.019: Contador, while

---
+++ tema02-estructurasDeControl/019-ContarWhile.cs
@@ -0,0 +1,20 @@
+﻿/* Ivan (...)
+ * 19. Crea un programa que muestre los números del 10 al 20, separados por un 
+ * espacio, sin avanzar de línea, usando "while".
+ */
+ 
+using System;
+
+class Contador
+{
+    static void Main()
+    {
+        int n = 10;
+        while (n <= 20)
+        {
+            Console.Write("{0} ", n);
+            n = n + 1;
+        }
+        Console.WriteLine();
+    }
+}
