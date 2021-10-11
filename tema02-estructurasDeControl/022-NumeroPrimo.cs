e7b4e5796060beb371a5dfeeffb219f5b348b4c1
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-10-11 09:36:21 +0200

    Ej.022: Número primo

---
+++ tema02-estructurasDeControl/022-NumeroPrimo.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * VIRGINIA (...)
+ * 22. Crea un programa que le pida al usuario un número y 
+ * responda si es primo. Debes hacerlo contando cuántos 
+ * divisores tiene. Será primo si y solo si tiene exactamente 
+ * dos divisores.
+ */
+
+using System;
+
+class Ejercicio22
+{
+    static void Main()
+    {
+        int numero, divisorActual=1, divisores=0;
+
+        Console.Write("Teclea un número entero: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        while (divisorActual <= numero)
+        {
+            if (numero % divisorActual == 0)
+            {                 
+                divisores++;
+            }
+            divisorActual++;
+        }
+
+        if (divisores == 2)
+        {
+            Console.WriteLine("Es primo.");
+        }
+        else
+        {
+            Console.WriteLine("No es primo.");
+        }
+
+    }
+}
