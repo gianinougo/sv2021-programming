4e55e95da4ea4debc12474d7f1729f5bed5bc2a4
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-10-17 12:23:57 +0200

    Ej.029: Rectángulo de asteriscos

---
+++ tema02-estructurasDeControl/029-Rectangulo.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * VIRGINIA (...)
+ * 
+ * 29. Haz un programa que pida al usuario un alto y un ancho 
+ * y que muestre un rectángulo formado por asteriscos, con ese 
+ * alto y ancho, como en este ejemplo:
+ * Alto? 5
+ * Ancho? 3
+ * ***
+ * ***
+ * ***
+ * ***
+ * ***
+ */
+
+using System;
+
+class Ejercicio29
+{
+    static void Main()
+    {
+        int alto, ancho, fila, columna;
+        Console.Write("Alto? ");
+        alto = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Ancho? ");
+        ancho = Convert.ToInt32(Console.ReadLine());
+
+        for (fila=1; fila<=alto; fila++)
+        {
+            for (columna=1; columna<=ancho; columna++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
