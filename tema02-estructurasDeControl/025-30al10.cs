7ad36466c12a03f4919d98984a79b711b2989c97
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-10-17 11:57:25 +0200

    Ej.025 Del 30 al 10, de 2 en 2

---
+++ tema02-estructurasDeControl/025-30al10.cs
@@ -0,0 +1,19 @@
+﻿//Víctor (...)
+
+// Ejercicio 25 T2. Muestra los números pares del 30 al 10, ambos inclusive, 
+// descendiendo, separados por un espacio, sin avanzar de línea, usando "for".
+
+using System;
+
+class numPar30a10
+{
+    static void Main()
+    {    
+        for(int i = 30; i >= 10; i = i-2)
+        {
+            Console.Write("{0} ", i);
+        }
+        Console.WriteLine(); 
+    }
+}
+
