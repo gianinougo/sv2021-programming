b18d982a373ff03a58603cc63c3c852fe0bbd243
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-10-17 12:24:25 +0200

    Ej.030: Triángulo decreciente

---
+++ tema02-estructurasDeControl/030-TrianguloDecreciente.cs
@@ -0,0 +1,37 @@
+﻿/*
+ *  Alejandro (...)
+ *  
+ *  30. Muestra un triángulo decreciente alineado a la izquierda, con el tamaño
+ *  indicado por el usuario, usando "for":
+        Tamaño? 5
+        *****
+        ****
+        ***
+        **
+        *
+ */
+
+using System;
+class Ejercicio_30
+{
+    static void Main()
+    {
+        int filas, columnas, tamanyo, asteriscos;
+
+        Console.Write("Tamaño? ");
+        tamanyo = Convert.ToInt32(Console.ReadLine());
+
+        asteriscos = tamanyo;
+
+        for (filas = 0; filas < tamanyo; filas++)
+        {
+            for (columnas = 0; columnas < asteriscos; columnas++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            asteriscos--;
+         }
+    }
+}
