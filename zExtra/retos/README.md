0f611d1f9e517af206314dae8470a11d1cb9a70d
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-10-23 12:14:36 +0200

    Ejemplo de solución al reto 01.01

---
+++ zExtra/retos/README.md
@@ -0,0 +1,7 @@
+# sv2021-programming
+
+## retos
+
+Ejemplos de soluciones a retos de programación
+
+
---
+++ zExtra/retos/Reto-01-01a-HolaMundo.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * VIRGINIA (...)
+ * 
+ * Entrada
+ * La entrada consta de una única línea que contiene un número n, 0 ≤ n ≤ 5, 
+ * que indica cuántos mensajes hay que emitir.
+ * 
+ * Salida
+ * Cada mensaje a escribir aparecerá en una única línea y será la cadena 
+ * "Hola mundo.".
+ */
+
+using System;
+
+class Reto1_01
+{
+    static void Main()
+    {
+        int n,i;
+        
+        n = Convert.ToInt32(Console.ReadLine());
+      
+        for (i = 1; i <= n; i++)
+        {
+            Console.WriteLine("Hola mundo.");
+        }                          
+    }
+}
