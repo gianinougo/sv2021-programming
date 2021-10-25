d69eaa0f2c9dfbee6205498c66a539c9738a12ad
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-10-25 10:02:02 +0200

    Ej.035: Cambio (dos versiones)

---
+++ tema02-estructurasDeControl/035a-Devolucion1.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * VIRGINIA (...)
+ * 35. Crea un programa devuelva el cambio de una compra, utilizando siempre 
+ * en primer lugar monedas (o billetes) del valor más grande posible. 
+ * Supondremos que tenemos una cantidad ilimitada de monedas (o billetes) de 
+ * 50, 10, 5 y 1, y que no hay decimales. La ejecución podría ser algo como :
+ * Precio? 222
+ * Pagado? 300
+ * Tu cambio es 78: 50 10 10 5 1 1 1 
+ */
+
+using System;
+
+class Ejercicio35
+{
+    static void Main()
+    {
+        int precio, pagado, cambio;
+
+        Console.Write("Precio? ");
+        precio = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Pagado? ");
+        pagado = Convert.ToInt32(Console.ReadLine());
+
+        cambio = pagado - precio; 
+
+        Console.Write("Tu cambio es {0}:", pagado - precio);
+
+        while (cambio >= 50)
+        {
+            Console.Write(" 50 ");
+            cambio = cambio - 50;
+        }
+        
+        while (cambio >= 10)
+        {
+            Console.Write(" 10 ");
+            cambio = cambio - 10;
+        }
+        
+        while (cambio >= 5)
+        {
+            Console.Write(" 5 ");
+            cambio = cambio - 5;
+        }
+        
+        while (cambio >= 1)
+        {
+            Console.Write(" 1 ");
+            cambio = cambio - 1;
+        }
+        
+        Console.WriteLine();
+    }
+}
---
+++ tema02-estructurasDeControl/035b-Devolucion2.cs
@@ -0,0 +1,42 @@
+﻿/*35.Programa devuelva el cambio de una compra, utilizandosiempre en primer 
+lugar monedas (o billetes) del valor más grande posible
+Rocio (...) */
+using System;
+class Devolucion35
+{
+    static void Main()
+    {
+        Console.Write("Introduzca el precio a pagar: ");
+        int precio = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Introduzca el efectivo entregado: ");
+        int pagado = Convert.ToInt32(Console.ReadLine());
+        int devolucion = pagado - precio;
+        
+        
+        Console.WriteLine("Total a devolver: "+devolucion);
+        while (devolucion != 0)
+        {
+            if (devolucion >= 50)
+            {
+                Console.Write("50 ");
+                devolucion = devolucion - 50;
+            }
+            else if (devolucion >= 10)
+            {
+                Console.Write("10 ");
+                devolucion = devolucion - 10;
+            }
+            else if (devolucion >= 5)
+            {
+                Console.Write("5 ");
+                devolucion = devolucion - 5;
+            }
+            else if (devolucion >= 1)
+            {
+                Console.Write("1 ");
+                devolucion = devolucion - 1;
+            }
+        }
+        Console.WriteLine();
+    }
+}
