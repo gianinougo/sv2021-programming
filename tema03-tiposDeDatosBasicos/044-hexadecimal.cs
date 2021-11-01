5dfdd352ce5a572ca9fcda1132a6da61b5c7b24a
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-01 14:31:22 +0100

    Ej.044: Mostrar en hexadecimal

---
+++ tema03-tiposDeDatosBasicos/044-hexadecimal.cs
@@ -0,0 +1,28 @@
+﻿//44. Programa que muestre todos los números entre ellos,en hexadecimal.
+//Rocio (...)
+
+using System;
+
+class Hexadecimal44
+{
+    static void Main()
+    {
+        Console.Write("Introduce un numero: ");
+        byte primero = Convert.ToByte(Console.ReadLine());
+        Console.Write("Introduce otro numero: ");
+        byte segundo = Convert.ToByte(Console.ReadLine());
+
+        if (segundo <= primero)
+        {
+            byte menor = segundo;
+            segundo = primero;
+            primero = menor;
+        }
+
+        for (int i = primero; i <= segundo; i++)
+        {
+            Console.Write(Convert.ToString(i, 16));
+            Console.Write(" ");
+        }
+    }
+}
