de5bd3bd9256f448bd516ec4aaf5877081735af7
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-07 23:39:00 +0100

    Ej.051: Login y password

---
+++ tema03-tiposDeDatosBasicos/051-loginPassword.cs
@@ -0,0 +1,44 @@
+﻿/* Juan Manuel (...) */
+
+/*Ejercicio51.Realiza una nueva versión del programa de la contraseña
+ *de acceso con 3 intentos, pero esta vez pidiendo un login (que será
+ *una cadena de texto) y también una clave (otra cadena de texto).*/
+
+using System;
+
+class Ejercicio51
+{
+    static void Main()
+    {
+
+        string login, clave;
+        bool accesoPermitido = false;
+        const string claveAcceso = "The Boss";
+        const string loginAcceso = "El Jefe";
+        int intentos = 0;
+        
+        do
+        {
+            intentos++;
+            Console.Write("Deme su login :");
+            login = Console.ReadLine();
+
+            Console.Write("Deme su clave se acceso :");
+            clave = Console.ReadLine();
+            if (login == loginAcceso && clave == claveAcceso)
+            {
+                accesoPermitido = true;
+            }
+        }
+        while (intentos < 3 && ! accesoPermitido);
+
+        if (accesoPermitido)
+        {
+            Console.WriteLine("Acceso concedido.");
+        }
+        else
+        {
+            Console.WriteLine("Acceso denegado.");
+        }
+    }
+}
