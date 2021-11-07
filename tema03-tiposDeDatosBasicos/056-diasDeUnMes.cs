21be751aba729fd43879602de3f4ff59d1195e03
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-07 23:50:28 +0100

    Ej.056: Días de un mes, a partir de su nombre

---
+++ tema03-tiposDeDatosBasicos/056-diasDeUnMes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/* Ivan (...)
+56. Haz un programa que pida al usuario el nombre de un mes (por ejemplo "marzo") y le 
+responda cuántos días tiene (usando el formato "marzo tiene 31 días").
+*/
+
+class Program
+{
+    static void Main()
+    {
+        string mes;
+
+        Console.WriteLine("Ingrese el nombre de un mes");
+        mes = Console.ReadLine();
+
+        switch(mes)
+        {
+
+            case "Enero":
+            case "Marzo":
+            case "Mayo":
+            case "Julio":
+            case "Agosto":
+            case "Octubre":
+            case "Diciembre":
+                Console.WriteLine("El mes {0} tiene 31 días", mes);
+                break;
+            case "Febrero":
+                Console.WriteLine("El mes {0} tiene 28 días", mes);
+                break;
+            default: Console.WriteLine("El mes {0} tiene 30 días", mes);
+                break;
+        }
+    }
+}
