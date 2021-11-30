56019751351315ebe0c6e0255d38daa4c87c0ece
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-30 11:12:05 +0100

    Ej.029b y 038c: Rectángulos, con "new string"

---
+++ tema04-arraysStructs/029b-RectanguloNewString.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *
+ * 29. Haz un programa que pida al usuario un alto y un ancho 
+ * y que muestre un rectángulo formado por asteriscos, con ese 
+ * alto y ancho, como en este ejemplo:
+ * Alto? 5
+ * Ancho? 3
+ * ***
+ * ***
+ * ***
+ * ***
+ * ***
+ * 
+ * Versión 2: con "new string"
+ * 
+ */
+
+using System;
+
+class Ejercicio29b
+{
+    static void Main()
+    {
+        int alto, ancho, fila;
+        Console.Write("Alto? ");
+        alto = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Ancho? ");
+        ancho = Convert.ToInt32(Console.ReadLine());
+        
+        string lineaAsteriscos = new string('*', ancho);
+
+        for (fila=1; fila<=alto; fila++)
+        {
+            Console.WriteLine(lineaAsteriscos);
+        }
+    }
+}
+
---
+++ tema04-arraysStructs/038c-RectanguloHueco3NewString.cs
@@ -0,0 +1,27 @@
+﻿// Ejercicio recomendado 38
+// Dibuja un rectángulo hueco con un número como símbolo
+
+// Versión alternativa con "new string"
+
+using System;
+
+class RectanguloHueco3
+{
+    static void Main()
+    {
+        Console.WriteLine("Digame el ancho");
+        int ancho = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digame el alto");
+        int alto = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digame el simbolo");
+        char simbolo = Convert.ToChar(Console.ReadLine());
+        
+        string lineaAsteriscos = new string(simbolo, ancho);
+        string lineaCentral = simbolo + new string(' ', ancho-2) + simbolo;
+        
+        Console.WriteLine(lineaAsteriscos);
+        for (int fila = 1; fila <= alto - 2; fila++)
+            Console.WriteLine(lineaCentral);
+        Console.WriteLine(lineaAsteriscos);
+    }
+}
