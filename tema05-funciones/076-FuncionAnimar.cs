18c2a7c4f9d061aba965411301064983294b4774
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-12-05 15:50:42 +0100

    Ej.076: Función Animar

---
+++ tema05-funciones/076-FuncionAnimar.cs
@@ -0,0 +1,24 @@
+﻿/*
+ *  Alejandro (...)
+ *  
+ *  Ejercicio_76: Frunción Animar
+ * 
+ * Crea una función llamada "Animar", que escriba en pantalla "Voy a aprobar 
+ * todo". Úsala desde Main, en un programa de prueba, en el que la función esté 
+ * declarada después de Main.
+ */
+
+using System;
+
+class Ejercicio_76
+{
+    static void Main()
+    {
+        Animar();
+    }
+
+    static void Animar()
+    {
+        Console.WriteLine("Voy a aprobar todo");
+    }
+}
