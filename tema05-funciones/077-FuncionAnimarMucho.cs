d3aae5c63f0df7f1d8ec5b8456eaca9bb3d5dd01
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-12-05 15:50:55 +0100

    Ej.077: Función AnimarMucho

---
+++ tema05-funciones/077-FuncionAnimarMucho.cs
@@ -0,0 +1,37 @@
+﻿/* Juan Manuel (...) 1º DAM Sem.*/
+
+/* Ejercicio77.Crea una función llamada "AnimarMucho", que escriba
+ * "Voy a aprobar " seguido del texto indicado en un primer 
+ * parámetro (string). Esa frase se repetirá tantas veces como se
+ * indique en un segundo parámetro numérico entero. 
+ * Aplícala a un programa de prueba, que pregunte el nombre de la 
+ * asignatura (o módulo) que sabes que vas a aprobar. En ese programa
+ * de prueba, tu función estará declarada antes de Main.*/
+
+
+using System;
+
+
+class Ejercicio77
+{
+
+    static void AnimarMucho(string asignatura, int repetirVeces)
+    {
+        for (int i = 0; i < repetirVeces; i++)
+        {
+            Console.WriteLine("Voy a aprobar " + asignatura);
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Dime la asignatura : ");
+        string asignatura = Console.ReadLine();
+        Console.WriteLine("Dime las repetiones del texto : ");
+        int repetirVeces = Convert.ToInt32(Console.ReadLine());
+        
+        AnimarMucho(asignatura, repetirVeces);
+    }
+}
+
