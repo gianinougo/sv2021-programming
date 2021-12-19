41ce709b11df149d9b0b26f95fb9f48520b52e87
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-12-20 00:01:02 +0100

    Ej.085: Función GetMaxMin

---
+++ tema05-funciones/085b-FuncionGetMaxMinRef2.cs
@@ -0,0 +1,57 @@
+﻿//Francis (...), retoques por Nacho
+
+/* 85. Crea una función llamada "GetMinMax", que devolverá el mínimo y el máximo
+ * del array de números reales de simple precisión que se pase como parámetro. 
+ * Si el array está vacío, no devolverá ningún valor, sino que se deberá lanzar 
+ * una excepción (para lo que no necesitas hacer nada especial: el programa 
+ * fallará al acceder a la posición [0], y es un comportamiento aceptable). Crea 
+ * un Main de prueba.*/
+
+
+using System;
+class ejercicio_85
+{
+    static void GetMinMax(float[] a, ref float min, ref float max)
+    {
+        max = min = a[0];
+        for (int i = 1; i < a.Length; i++)
+        {
+            if (a[i] > max)
+            {
+                max = a[i];
+            }
+
+            if (a[i] < min)
+            {
+                min = a[i];
+            }
+        }
+    }
+
+    static void Main()
+    { 
+        Console.WriteLine("Longitud del array:");
+        int longitud = Convert.ToInt32(Console.ReadLine());
+        float[] array = new float[longitud];
+
+        float minimo = 0;
+        float maximo = 0;
+
+        for (int i = 0; i < longitud; i++)
+        {
+            try
+            {
+                Console.WriteLine("Valor {0}:", i+1);
+                array[i] = Convert.ToSingle(Console.ReadLine());
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Número incorrecto");
+            }
+        }
+
+        GetMinMax(array, ref minimo, ref maximo);
+        Console.WriteLine("Min = {0}, max = {1}", minimo, maximo);
+    }
+}
---
+++ tema05-funciones/085c-FuncionGetMaxMinOut.cs
@@ -0,0 +1,46 @@
+﻿// Ejercicio recomendado 85
+// Javier (...)
+
+/* 85. Crea una función llamada "GetMinMax", que devolverá el mínimo y el máximo
+ * del array de números reales de simple precisión que se pase como parámetro. 
+ * Si el array está vacío, no devolverá ningún valor, sino que se deberá lanzar 
+ * una excepción (para lo que no necesitas hacer nada especial: el programa 
+ * fallará al acceder a la posición [0], y es un comportamiento aceptable). Crea 
+ * un Main de prueba.*/
+
+using System;
+
+class Ejercicio85
+{
+    static void Main()
+    {
+        const int MAX = 10;
+        Random generador = new Random();
+        float[] datos = new float[MAX];
+
+        for (int i = 0; i < MAX; i++)
+            datos[i] = (float)generador.NextDouble()*100;
+
+        float minimo, maximo;
+
+        GetMinMax(datos, out minimo, out maximo);
+        foreach (float d in datos)
+            Console.Write("{0} ", d.ToString("N3"));
+        Console.WriteLine();
+        Console.WriteLine("Mínimo: {0}", minimo.ToString("N3"));
+        Console.WriteLine("Máximo: {0}", maximo.ToString("N3"));
+    }
+
+    static void GetMinMax(float[] datos, out float min, out float max)
+    {
+        min = datos[0];
+        max = datos[0];
+        foreach (float d in datos)
+        {
+            if (min > d)
+                min = d;
+            if (max < d)
+                max = d;
+        }
+    }
+}
