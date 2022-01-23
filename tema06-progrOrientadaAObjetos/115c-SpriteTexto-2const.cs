f65d82e43fc06d34e7976b2f2dfaea9035189b49
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-01-23 19:51:29 +0100

    Ej.115: SpriteTexto (1 constructor y 2 constructores)

---
+++ tema06-progrOrientadaAObjetos/115c-SpriteTexto-2const.cs
@@ -0,0 +1,77 @@
+﻿// Ejercicio recomendado 115
+// Javier (...)
+
+using System;
+
+class SpriteTexto
+{
+    private int x, y;
+    private char caracter;
+
+    public SpriteTexto()
+    {
+    }
+
+    public SpriteTexto(int nuevoX, int nuevoY, char nuevoCaracter)
+    {
+        x = nuevoX;
+        y = nuevoY;
+        caracter = nuevoCaracter;
+    }
+    
+    public int GetX() { return x; }
+    public int GetY() { return y; }
+    public char GetCaracter() { return caracter; }
+    
+    public void SetX(int nuevoX) { x = nuevoX; }
+    public void SetY(int nuevoY) { y = nuevoY; }
+    public void SetCaracter(char nuevoCaracter) { caracter = nuevoCaracter; }
+
+    public void MoverDerecha()
+    {
+        x++;
+    }
+
+    public void MoverIzquierda()
+    {
+        x--;
+    }
+
+    public void Dibujar()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(caracter);
+    }
+
+}
+
+class PruebaDeSprite
+{
+    static void Main()
+    {
+        SpriteTexto s = new SpriteTexto();
+
+        s.SetX(40);
+        s.SetY(12);
+        s.SetCaracter('A');
+        s.Dibujar();
+        
+        Console.ReadLine();
+        s.MoverDerecha();
+        s.Dibujar();
+        
+        Console.ReadLine();
+        s.MoverDerecha();
+        s.Dibujar();
+        
+        Console.ReadLine();
+        s.MoverDerecha();
+        s.Dibujar();
+        
+        Console.ReadLine();
+        SpriteTexto s2 = new SpriteTexto(2, 20, '#');
+        s2.Dibujar();
+
+        Console.ReadLine();
+    }
+}
