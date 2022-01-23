a933602d00b28c2f7061ee5472b4ff94234686a3
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-01-23 19:45:08 +0100

    Ej.111: Recuuadro + Inicializar

---
+++ tema06-progrOrientadaAObjetos/111b-RecuadroInicializar.cs
@@ -0,0 +1,64 @@
+﻿// Rocio (...)
+
+using System;
+
+class Recuadro
+{
+    private int x, y, ancho, alto;
+    private char caracter;
+    
+    public int GetX() { return x; }
+    public int GetY() { return y; }
+    public int GetAncho() { return ancho; }
+    public int GetAlto() { return alto; }
+    public char GetCaracter() { return caracter; }
+    
+    public void SetX(int nuevoX) { x = nuevoX; }
+    public void SetY(int nuevoY) { y = nuevoY; }
+    public void SetAncho(int nuevoAncho) { ancho = nuevoAncho; }
+    public void SetAlto(int nuevoAlto) { alto = nuevoAlto; }
+    public void SetCaracter(char nuevoCaracter) { caracter = nuevoCaracter; }
+    
+    public void Inicializar(int valorX, int valorY, 
+        int valorAncho, int valorAlto,char valorCaracter) 
+    {
+        x =valorX;
+        y = valorY;
+        ancho = valorAncho;
+        alto = valorAlto;
+        caracter = valorCaracter;
+    }
+
+    public void Dibujar()
+    {
+        for (int fila=y; fila<=y+alto-1; fila++)
+        {
+            for (int col=x; col<=x+ancho-1; col++)
+            {
+                if ((fila==y)||(fila==y+alto-1) 
+                    || (col==x)||(col==x+ancho-1))
+                {
+                    Console.SetCursorPosition(col, fila);
+                    Console.Write(caracter);
+                }
+            }
+        }
+    }
+}
+
+// ---------------------
+
+class PruebaDeRecuadro
+{
+    static void Main()
+    {
+        Recuadro prueba = new Recuadro();
+        prueba.Inicializar(1,4,8,10, '*');
+        prueba.Dibujar();
+        
+        Recuadro prueba2 = new Recuadro();
+        prueba2.Inicializar(20, 10, 20, 5, '+');
+        prueba2.Dibujar();
+    }
+}
+
