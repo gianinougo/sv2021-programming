32616e0432903baca11600f46f8d61ec74d448a5
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-01-23 19:52:00 +0100

    Ej.116: SpriteTexto + SpriteTextoColor

---
+++ tema06-progrOrientadaAObjetos/116b-SpriteTextoColor.cs
@@ -0,0 +1,96 @@
+﻿// Ejercicio recomendado 116
+// Javier (...)
+
+using System;
+
+class SpriteTexto
+{
+    protected int x, y;
+    protected char caracter;
+
+    public SpriteTexto()
+    {
+
+    }
+    public SpriteTexto(int nuevoX, int nuevoY, char nuevoCaracter)
+    {
+        x = nuevoX;
+        y = nuevoY;
+        caracter = nuevoCaracter;
+    }
+    
+    public int GetX() { return x; }
+    public int GetY() { return y; }
+    public char GetCaracter() { return caracter; }
+    
+    public void SetX(int nuevoX) { x = nuevoX; }
+    public void SetY(int nuevoY) { y = nuevoY; }
+    public void SetCaracter(char nuevoCaracter) { caracter = nuevoCaracter; }
+
+    public void MoverDerecha()
+    {
+        x++;
+    }
+
+    public void MoverIzquierda()
+    {
+        x--;
+    }
+
+    public void Dibujar()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(caracter);
+    }
+
+}
+
+// -----------------------
+
+class SpriteTextoColor : SpriteTexto
+{
+    private string color;
+
+    public SpriteTextoColor(int nuevoX, int nuevoY, char nuevoCaracter, 
+        string nuevoColor)
+    {
+        x = nuevoX;
+        y = nuevoY;
+        caracter = nuevoCaracter;
+        color = nuevoColor;
+    }
+
+    public new void Dibujar()
+    {
+        switch (color)
+        {
+            case "blanco": Console.ForegroundColor = ConsoleColor.White; break;
+            case "cyan": Console.ForegroundColor = ConsoleColor.Cyan; break;
+            case "amarillo": Console.ForegroundColor = ConsoleColor.Yellow; break;            
+        }
+
+        Console.SetCursorPosition(x, y);
+        Console.Write(caracter);
+        Console.ResetColor();
+    }
+}
+
+// -----------------------
+
+class PruebaDeSprite
+{
+    static void Main()
+    {
+        SpriteTexto s = new SpriteTexto();
+        s.SetX(40);
+        s.SetY(12);
+        s.SetCaracter('A');
+        s.Dibujar();
+        
+        SpriteTexto s2 = new SpriteTexto(2, 20, '#');
+        s2.Dibujar();
+
+        SpriteTextoColor s3 = new SpriteTextoColor(30, 8, 'X', "amarillo");
+        s3.Dibujar();
+    }
+}
