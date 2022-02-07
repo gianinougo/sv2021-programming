ce0ffe10c15582a2c79c4ff81a081fd671cd4ada
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-02-07 09:36:35 +0100

    Ej.132 Recuadro + array + this

---
+++ tema06-progrOrientadaAObjetos/132-RecuadroArrayThis.cs
@@ -0,0 +1,89 @@
+﻿// Ejercicio 132: Recuadro + RecuadroRelleno + "this"
+
+using System;
+
+class Recuadro
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Ancho { get; set; }
+    public int Alto { get; set; }
+    public char Caracter { get; set; }
+    
+    public Recuadro(int valorX, int valorY, 
+        int valorAncho, int valorAlto,char valorCaracter) 
+    {
+        X = valorX;
+        Y = valorY;
+        Ancho = valorAncho;
+        Alto = valorAlto;
+        Caracter = valorCaracter;
+    }
+    
+    public Recuadro(int valorX, int valorY) 
+        :this(valorX, valorY, 40, 10, '*')
+    {
+    }
+
+    public virtual void Dibujar()
+    {
+        for (int fila = Y; fila <= Y + Alto - 1; fila++)
+        {
+            for (int col = X; col <= X + Ancho - 1; col++)
+            {
+                if ((fila==Y)||(fila==Y+Alto-1) 
+                    || (col==X)||(col==X+Ancho-1))
+                {
+                    Console.SetCursorPosition(col, fila);
+                    Console.Write(Caracter);
+                }
+            }
+        }
+    }
+}
+
+// ---------------------
+
+class RecuadroRelleno : Recuadro
+{
+    public RecuadroRelleno(int valorX, int valorY, 
+            int valorAncho, int valorAlto, char valorCaracter)
+        : base (valorX, valorY, valorAncho, valorAlto, valorCaracter)
+    {
+    }
+    
+    public override void Dibujar()
+    {      
+        for (int fila = Y; fila <= Y + Alto - 1; fila++)
+        {
+            for (int col = X; col <= X + Ancho - 1; col++)
+            {
+                Console.SetCursorPosition(col, fila);
+                Console.Write(Caracter);
+            }
+        }
+    }
+}
+
+// ---------------------
+
+class PruebaDeRecuadro
+{
+    static void Main()
+    {
+        Recuadro[] recuadros = new Recuadro[4];
+        recuadros[0] = new Recuadro(1,4,8,10, 'x');
+        recuadros[0].X = 5;
+        recuadros[0].Caracter = 'M';
+        
+        recuadros[1] = new Recuadro(20, 10, 20, 5, '+');
+        recuadros[2] = new RecuadroRelleno(18, 12, 8, 8, '-');
+        recuadros[3] = new Recuadro(2, 3);
+        
+        for (int i = 0; i < 4; i++)
+        {
+            recuadros[i].Dibujar();
+        }
+    }
+}
+
