b12cb95e1c3beb624bb4be9981616739cf9c4f83
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-02-20 17:14:30 +0100

    Ejercicio de recursividad R06: Permutaciones

---
+++ zExtra/recursividad/R06.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Permutaciones
+{
+    static void EscribirPermutaciones(string cadenaRestante, string letrasUsadas)
+    {
+        if (cadenaRestante.Length == 0) Console.WriteLine(letrasUsadas);
+        else for (int i = 0; i < cadenaRestante.Length; i++)
+                EscribirPermutaciones(cadenaRestante.Remove(i,1), 
+                    letrasUsadas+cadenaRestante[i]);
+    }
+    
+    static void EscribirPermutaciones(string cadena)
+    {
+        EscribirPermutaciones(cadena, "");
+    }
+    
+    static void Main()
+    {
+        EscribirPermutaciones("123");
+        EscribirPermutaciones("hola");
+    }
+}
