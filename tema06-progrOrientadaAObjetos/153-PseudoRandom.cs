db175aa9e17d57c91c7c033fd3af993cd9bdb7bd
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-02-21 12:43:25 +0100

    Ej.153: Clase PseudoRandom

---
+++ tema06-progrOrientadaAObjetos/153-PseudoRandom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+153. Crea una clase llamada "PseudoRandom" para generar "falsos números 
+aleatorios". En ella, crea un método estático "Get" que devolverá los 
+milisegundos del reloj del sistema actual (deberás usar 
+"DateTime.Now.Millisecond"). Crea un "Main" para probarla.
+*/
+
+// Rocío (...)
+
+class PseudoRandom
+{
+    public static int Get()
+    {
+       return DateTime.Now.Millisecond;
+    }
+}
+
+// ---------
+
+class Prueba
+{
+    static void Main()
+    {
+        Console.WriteLine("Al azar... " + PseudoRandom.Get());
+    }
+}
