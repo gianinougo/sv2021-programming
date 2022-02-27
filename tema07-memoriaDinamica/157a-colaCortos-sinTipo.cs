21ed2ad2630b9291fd21fe6ec2d38d82e8bd658e
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-02-27 23:58:43 +0100

    Ej.157: Cola de enteros cortos

---
+++ tema07-memoriaDinamica/157a-colaCortos-sinTipo.cs
@@ -0,0 +1,63 @@
+﻿/* Juan manuel (...) 1º DAM Sem. */
+
+/* 
+Ejercicio 157.Pide al usuario números enteros cortos (short), tantos como
+desee, hasta que pulse Intro en vez de un número. Deberás ir almacenando
+todos ellos en una cola. Finalmente, deberás mostrar todos los datos que
+ha introducido, así como su suma y su media (que deberás calcular en el
+momento de mostrar los datos, no en el de introducirlos). Puedes emplear
+una cola con tipo base -Generics- o sin tipo, como prefieras (se compartirán
+ambas soluciones, para que puedas comparar).
+
+ *****  SIN TIPO  *****
+
+*/
+
+
+using System;
+using System.Collections;
+
+
+class Ejercicio157a
+{
+    static void Main()
+    {
+        Queue miCola = new Queue();
+        string cadenaNumero;
+        Console.WriteLine("Deme todos los numeros que desee separados");
+        Console.WriteLine("por Enter, para finalizar pulse intro : ");
+        do
+        {
+            cadenaNumero = Console.ReadLine();
+            if (cadenaNumero != "")
+            {
+                miCola.Enqueue(Convert.ToInt16(cadenaNumero));
+            }
+        } while (cadenaNumero != "");
+
+        int elementosCola = miCola.Count;
+        int suma = 0;
+
+        for (int i = 0; i < elementosCola; i++)
+        {
+            short dato = (short) (miCola.Dequeue());
+            suma += dato;
+            if (miCola.Count >= 1)
+            {
+                Console.Write("{0}, ", dato);
+            }
+            else
+            {
+                Console.WriteLine("{0}", dato);
+            }
+
+        }
+        Console.WriteLine("La suma de los elementos de la cola" +
+            " es : {0}", suma);
+        Console.WriteLine("La media de los elementos de la cola" +
+            " es : {0}", suma / elementosCola);
+
+    }
+}
+
+
