df708917c2dc3514b692ee2fe09ecc3ea4a472fe
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-02-28 00:01:40 +0100

    Ej.164: Fichero invertido usando una pila

---
+++ tema07-memoriaDinamica/164a-InvertirFichero1.cs
@@ -0,0 +1,35 @@
+﻿/* Juan Manuel (...) 1º DAM Sem.*/
+
+/*
+Ejercicio 164.Crea un programa que muestre el contenido de un fichero de texto,
+invertido (de la última línea a la primera), de la siguiente forma:
+
+Para leer todo el contenido de un fichero a un array de strings puedes hacer
+"string[] lineas = File.ReadAllLines("nombreDeFichero.ext");"
+(necesitarás añadir también "using System.IO;")
+Puedes crear una pila a partir del contenido de un array si indicas dicho
+array como parámetro del constructor:
+"Stack<string> pila = new Stack<string>(lineas);"
+Ya sólo queda que recorras la pila, mostrando la información que contiene.
+*/
+
+// Versión 1: con "Pop"
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Ejercicio164a
+{
+    static void Main()
+    {
+        Console.Write("Dime el nombre del fichero: ");
+        string rutaFichero = Console.ReadLine();
+
+        string[] lineas = File.ReadAllLines(rutaFichero);
+        Stack<string> pila = new Stack<string>(lineas);
+        while(pila.Count > 0)
+                Console.WriteLine(pila.Pop());
+    }
+}
+
---
+++ tema07-memoriaDinamica/164b-InvertirFichero2.cs
@@ -0,0 +1,37 @@
+﻿/* Juan Manuel (...) 1º DAM Sem.*/
+
+/*
+Ejercicio 164.Crea un programa que muestre el contenido de un fichero de texto,
+invertido (de la última línea a la primera), de la siguiente forma:
+
+Para leer todo el contenido de un fichero a un array de strings puedes hacer
+"string[] lineas = File.ReadAllLines("nombreDeFichero.ext");"
+(necesitarás añadir también "using System.IO;")
+Puedes crear una pila a partir del contenido de un array si indicas dicho
+array como parámetro del constructor:
+"Stack<string> pila = new Stack<string>(lineas);"
+Ya sólo queda que recorras la pila, mostrando la información que contiene.
+*/
+
+// Versión 2: con "foreach"
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+class Ejercicio164b
+{
+    static void Main()
+    {
+        Console.Write("Dime el nombre del fichero: ");
+        string rutaFichero = Console.ReadLine();
+
+        string[] lineas = File.ReadAllLines(rutaFichero);
+        Stack<string> pila = new Stack<string>(lineas);
+
+        foreach(string linea in pila)
+            Console.WriteLine(linea);
+    }
+}
+
