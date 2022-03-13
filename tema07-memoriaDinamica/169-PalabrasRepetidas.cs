d7e9e9a12a1ad5b21566c406b38ede932b1f133e
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-03-13 17:03:46 +0100

    Ej.169: Palabras repetidas en un fichero

---
+++ tema07-memoriaDinamica/169-PalabrasRepetidas.cs
@@ -0,0 +1,72 @@
+﻿//Author: Franco (..) + Javier (...)
+
+/*
+/* Variante del ejercicio 167 que no muestre las frases repetidas sino las 
+ * palabras repetidas (ayudándote de ".Split()" para obtener las palabras 
+ * y de ".Replace" para eliminar los símbolos de puntuación más habituales. 
+ * Para que la salida no sea exageradamente grande, muestra sólo las palabras 
+ * que se repiten más de 500 veces. Al igual que antes, crea una versión con 
+ * SortedList y otra con una tabla Hash (o un Dictionary).
+*/
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Ej169
+{
+    static void Main()
+    {
+        Console.Write("Introduce el nombre del fichero: ");
+        string ficheroALeer = Console.ReadLine();
+        string[] lineas = File.ReadAllLines(ficheroALeer);
+        string signosPuntuacion = ",.;:-_´¨+*`^{}[]'¡¿?=!\"&%$#@|\\() ";
+        char[] separadores = signosPuntuacion.ToCharArray();
+
+        Console.WriteLine("Version con SortedList:");
+        SortedList<string, int> diccionario = new SortedList<string, int>();
+        DateTime comienzo = DateTime.Now;
+        foreach (string line in lineas) 
+        {
+            string[] palabras = line.Split(separadores);
+            foreach(string p in palabras)
+            {
+                if (diccionario.ContainsKey(p))
+                    diccionario[p] += 1;
+                else
+                    diccionario.Add(p, 1);
+            }
+        }
+        Console.WriteLine("Tiempo transcurrido: " + (DateTime.Now - comienzo));
+        for (int i = 0; i < diccionario.Count; i++)
+            if ((diccionario.Keys[i] != "") 
+                && (diccionario.Values[i] > 500))
+            {
+                Console.WriteLine("{0} = {1}",
+                    diccionario.Keys[i], diccionario.Values[i]);
+            }
+
+        Console.WriteLine();
+        Console.WriteLine("Version con tabla Hash:");
+        Dictionary<string, int> hashtable = new Dictionary<string, int>();
+        comienzo = DateTime.Now;
+        foreach (string line in lineas)
+        {
+            string[] palabras = line.Split(separadores);
+            foreach(string p in palabras)
+            {
+                if (hashtable.ContainsKey(p))
+                    hashtable[p] += 1;
+                else 
+                    hashtable.Add(p, 1);
+            }
+        }
+        Console.WriteLine("Tiempo transcurrido: " + (DateTime.Now - comienzo));
+        foreach (KeyValuePair<string, int> dic in hashtable)
+            if ((dic.Key != "") && (dic.Value > 500))
+                {
+                    Console.WriteLine("{0} = {1}", 
+                        dic.Key, dic.Value);
+                }
+    }
+}
