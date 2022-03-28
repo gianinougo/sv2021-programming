691f98a0bbea408e3faa02c294018b4b9f34cbb2
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-03-28 08:13:50 +0200

    Ej.187: Extraer caract. imprimibles 2 (FS)

---
+++ tema08-ficheros/187a-ExtraerImprimible2a.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System;
+/*
+ * 187. Crea un programa que extraiga los caracteres imprimibles 
+ * (código ASCII 32 a 127, además del 7, el 10 y el 13) de un fichero y los 
+ * vuelque a otro fichero. Todos los demás caracteres se convertirán a espacio 
+ * (ASCII 32). El nombre del fichero de origen se tomará de la línea de comandos
+ * y el de destino se creará añadiendo ".2.txt" al nombre de origen. Debes usar 
+ * FileStream y un bloque con el tamaño de todo el archivo. Si el fichero de 
+ * destino ya existe, lo sobreescribirás sin preguntar.
+ * 
+ * Rocio (...)
+ */
+
+class Ejercicio187
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Indique el nombre de fichero");
+            return;
+        }
+        string entrada = args[0];
+        Console.WriteLine("Nombre del formato del archivo");
+        string salida = entrada + ".2.txt";
+
+        FileStream f = File.OpenRead(entrada);
+        FileStream fileOut = File.Create(salida);
+        int maximo = (int)f.Length;
+       
+        byte[] datos = new byte[maximo];
+        f.Read(datos, 0, maximo);
+        for (int i = 0; i < maximo; i++)
+        {
+            if ((datos[i] >= 32 && datos[i] <= 127) || datos[i] == 7 ||
+                datos[i] == 10 || datos[i] == 13)
+            {
+                fileOut.WriteByte(datos[i]);
+            }
+            else
+            {
+                fileOut.WriteByte(32);
+
+            }
+        }
+        fileOut.Close();
+        f.Close();
+    }
+}
+
---
+++ tema08-ficheros/187b-ExtraerImprimible2b.cs
@@ -0,0 +1,78 @@
+﻿// Ejercicio recomendado 187
+// Javier (...), retoques por Nacho
+
+/* Crea un programa que extraiga los caracteres imprimibles 
+ * (código ASCII 32 a 127, además del 7, el 10 y el 13) de un fichero y los 
+ * vuelque a otro fichero. Todos los demás caracteres se convertirán a espacio 
+ * (ASCII 32). El nombre del fichero de origen se tomará de la línea de 
+ * comandos y el de destino se creará añadiendo ".2.txt" al nombre de origen.
+ * Debes usar FileStream y un bloque con el tamaño de todo el archivo. 
+ * Si el fichero de destino ya existe, lo sobreescribirás sin preguntar.
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class ExtraerCaracteresImprimibles2
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 1)
+        {
+            string nombreFichero = args[0];
+
+            if (File.Exists(nombreFichero))
+            {
+                try
+                {
+                    FileStream entrada = File.OpenRead(nombreFichero);
+                    int tamanyo = (int)entrada.Length;
+                    byte[] datos = new byte[tamanyo];   
+                    int cantidadLeida = entrada.Read(datos, 0, tamanyo);
+                    entrada.Close();
+                    
+                    if (cantidadLeida < tamanyo)
+                    {
+                        Console.WriteLine("No se ha podido leer completo");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < tamanyo; i++)
+                        {
+                            byte d = datos[i];
+                            if ( ! (d == 7 || d == 10 || d == 13 ||
+                                (d >= 32 && d <= 127)))
+                            {
+                                datos[i] = 32;
+                            }
+                        }
+
+                        FileStream salida = File.Create(nombreFichero + ".2.txt");
+                        salida.Write(datos, 0, tamanyo);
+                        salida.Close();
+                    }
+
+                    Console.WriteLine("Extracción de caracteres imprimibles " +
+                        "finalizada");
+                }
+                catch (PathTooLongException ptee)
+                {
+                    Console.WriteLine("Error en el nombre: " + ptee.Message);
+                }
+                catch (IOException ioe)
+                {
+                    Console.WriteLine("Error de lectura/escritura, " + ioe.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+            else
+                Console.WriteLine("El fichero no existe");
+        }
+        else
+            Console.WriteLine("No se ha introducido el nombre del fichero");
+    }
+}
